739e1592e169a351a6221dfa21db583f9cce4cda
Vinicius de Souza Silva <105164572+ViniciusSouzaSilva@users.noreply.github.com>
2024-06-18 11:00:43 -0300

    Finalização

---
+++ script.js
@@ -0,0 +1,11 @@
+function confirmar(){
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let tel = document.getElementById("telefone").value
+    let data = document.getElementById("data_nascimento").value
+    let cidade = document.getElementById("cidade").value
+    let estado = document.getElementById("estado").value
+    let endereco = document.getElementById("endereco").value
+
+    alert(`Dados a serem cadastrados:\n\nNome: ${nome}\nEmail: ${email}\nTelefone: ${tel}\nData de nascimento: ${data}\nCidade: ${cidade}\nEstado: ${estado}\nEndereço: ${endereco}`)
+}
